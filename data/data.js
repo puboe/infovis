5a8964c989f6546c9c01083d1faf99b24421df77
Pablo Diaz <pablo.diazuboe@mercadolibre.com>
2016-05-07 13:58:17 -0300

    Tp data personal

--- data/data.js
+++ data/data.js
@@ -3,43 +3,61 @@ var data = d3.json("http://bananastic.ddns.net:3002/refuels.json",
   					if (err != null) {
   						alert("Hey! Something went wrong")
   					} else {
-    					console.log(data)
+    					// console.log(data)
     					show(data)
   					}
 			});
 
 function show(data) {
 	
-	console.log(data[0])
+	var minDate, maxDate;
+
+	data.forEach(function(elem) {
+		elem.date = new Date(elem.created_at);
+		if(minDate == undefined || minDate > elem.date) {
+			minDate = elem.date;
+		}
+		if(maxDate == undefined || maxDate < elem.date) {
+			maxDate = elem.date;
+		}
+	})
+
+	var nestByStation = d3.nest().key(function(d) { return d.station }).entries(data);
 
 	var cf = crossfilter(data);
 
-	console.log(cf)
+	var litresDim = cf.dimension(function(d) { return d.date; });
+	var litresGroup = litresDim.group().reduceSum(function(d) { return d.litres }),
+		litresPriceGroup = litresDim.group().reduceSum(function(d) { return d.amount / d.litres })
 
-	var dim = {};
-
-	dim.litres = cf.dimension(function(d) { return d.id; });
-
-	console.log(dim.litres)
-
-	var charts = {};
-
-	charts.litres = dc.barChart("#litres")
-						.width(430)
+	var litresChart = dc.barChart("#litres")
+						.width(900)
 						.height(300)
-    					.dimension(dim.litres)
-        				.group(dim.litres.group().reduceSum(function(d) { return d.litres }))
-        				.elasticY(true)
-    					.x(d3.scale.linear().domain(d3.extent(data, function(d) { return d.id; })))
-					    .xAxis();
+    					.dimension(litresDim)
+        				.group(litresGroup)
+        				// .elasticY(true)
+    					.x(d3.time.scale().domain([minDate, maxDate.setMonth(maxDate.getMonth()+1)])
+							.rangeRound([0, 10 * 90]))
+    					.elasticX(true)
+    					// .xAxis().ticks(5)
 
-	charts.price = dc.barChart("#price")
-						.width(430)
+    					// .centerBar(true)
+    					// .zoomScale(d3.extent(data, function(d) { return d.id; }))
+    					// .mouseZoomable(true)
+    					// .brushOn(false)
+    					// .controlsUseVisibility(true)
+
+    					// .renderHorizontalGridLines(true)
+					    
+					    // .colors(d3.scale.category20b())
+
+	var priceChart = dc.barChart("#price")
+						.width(930)
 						.height(300)
-    					.dimension(dim.litres)
-        				.group(dim.litres.group().reduceSum(function(d) { return d.amount / d.litres }))
+    					.dimension(litresDim)
+        				.group(litresPriceGroup)
         				.elasticY(true)
-    					.x(d3.scale.linear().domain(d3.extent(data, function(d) { return d.id; })))
+    					.x(d3.time.scale().domain([minDate, maxDate]))
 					    .xAxis();
 
 
